34aa7522f1f5245ccee4ccd389e041292f9152e0
Rohan H <rohanhunakuppi@gmail.com>
2025-12-18 21:51:25 +0530

    Feat: Adding Express server

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-console.log("Am Learning CI/CD using Github Actions");
+import express from 'express'
+
+const app = express()
+const port = 3000;
+app.use(express.json());
+
+app.get('/',(req,res)=>{
+    res.json({
+        'message':'Server is running...'
+    })
+})
+
+// Endpoint 2: Sample API
+app.get('/api/user', (req, res) => {
+  res.json({
+    id: 1,
+    name: 'John Doe',
+    role: 'Developer'
+  });
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
